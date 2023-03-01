7e01beefa791bef27d68c11ca22edefbbcf32a49
Артём <sly.watching.fox@gmail.com>
2023-03-01 21:30:18 +0300

    01/03/23

---
+++ LabsOrg/Lesson6/TAsk2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TAsk2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральные числа N и a:");
+            int N, a;
+            while (!int.TryParse(Console.ReadLine(), out N))
+            {
+                Console.WriteLine("Не верный ввод. Повторите");
+            } while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Не верный ввод. Повторите");
+            }
+            IEnumerable<int> seq;
+            seq = System.Linq.Enumerable.Range(N, a);
+            foreach (var x in seq)
+                Console.Write($"{x} ");
+        }
+        //Выполнить: Создайте последовательность из N целых чисел, начинающихся с a.a и N — это натуральные числа.
+    }
+}
---
+++ LabsOrg/Lesson6/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральные числа N и a:");
+            int N, w;
+            while (!int.TryParse(Console.ReadLine(), out N))
+            {
+                Console.WriteLine("Не верный ввод. Повторите");
+            } while (!int.TryParse(Console.ReadLine(), out w))
+            {
+                Console.WriteLine("Не верный ввод. Повторите");
+            }
+            IEnumerable<int> seq;
+            seq = System.Linq.Enumerable.Range(N, w).Select((a, b) => (a + b));
+            foreach (var x in seq)
+                Console.Write($"{x} ");
+        }
+        //Выполнить: Создайте последовательность из 9 целых чисел, начиная с 1 с шагом = 2.
+        //Указание: чтобы получить шаг, вы можете использовать лямбда-выражение... .Select((a, b)=> ...);. Подумайте, что нужно вставить вместо....
+    }
+}
