0e0463c7ecb9fe2b07a87d2a77c1a9f06b772bde
Артём <sly.watching.fox@gmail.com>
2023-03-01 20:42:46 +0300

    01.03.2023

--- LabsOrg/Lesson5/Task6/Program.cs
+++ LabsOrg/Lesson5/Task6/Program.cs
@@ -20,7 +20,7 @@
         }
         static int BitwiseSum(int num1, int num2)
         {
-            int digit1num1 = num1 / 10; 
+            int digit1num1 = num1 / 10;
             int digit2num1 = num1 % 10;
             int digit1num2 = num2 / 10;
             int digit2num2 = num2 % 10;
@@ -35,9 +35,14 @@
                 bitwiseSum2 = bitwiseSum2 % 10;
             }
             int bitwiseSum = bitwiseSum1 * 10 + bitwiseSum2;
-            
+
             return bitwiseSum;
 
         }
+        //Выполнить: Вводятся два двузначных целых числа.Создайте функцию BitwiseSum(), которая вычисляет их побитовую сумму по модулю 10. Например, побитовая сумма чисел 34 и 59 является числом 83 (3 + 5 = 8; 4 + 9 = 13, 13%10 = 3).
+        //Указание 1: Функция BitwiseSum() имеет два аргумента.
+        //Указание 2:Функция возвращает целочисленное значение int:
+        //static int BitwiseSum(...);
+
     }
 }
