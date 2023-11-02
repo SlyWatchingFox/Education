01327a468169da8909c56842ad09f4d39de274a5
Артём <sly.watching.fox@gmail.com>
2023-11-02 15:11:11 +0300

    02,11,23

---
+++ Practice/Avalonia/ObservableCollectionTest/ObservableCollectionTest/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+
+namespace ObservableCollectionTest.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    private string _item;
+    public string Item
+    {
+        get => _item;
+        set => _item = value;
+    }
+    public ObservableCollection<string> MyItems { get; set; } = new ObservableCollection<string>();
+    public void AddItem()
+    {
+        MyItems.Add(Item);
+    }
+}
---
+++ Practice/Avalonia/ObservableCollectionTest/ObservableCollectionTest/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ObservableCollectionTest.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
