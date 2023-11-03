f5f7640cbf8c851293876d702a4a55da73880d3c
Артём <sly.watching.fox@gmail.com>
2023-11-03 13:03:39 +0300

    03,11,23

--- Practice/Avalonia/ObservableCollectionTest/ObservableCollectionTest/ViewModels/MainViewModel.cs
+++ Practice/Avalonia/ObservableCollectionTest/ObservableCollectionTest/ViewModels/MainViewModel.cs
@@ -5,16 +5,23 @@ namespace ObservableCollectionTest.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    private string _item;
-    public string Item
+    private string _name;
+    private int _age;
+    public string Name
     {
-        get => _item;
-        set => this.RaiseAndSetIfChanged(ref _item, value);
+        get => _name;
+        set => this.RaiseAndSetIfChanged(ref _name, value);
     }
-    public ObservableCollection<string> MyItems { get; set; } = new ObservableCollection<string>();
-    public void AddItem()
+    public int Age
     {
-        MyItems.Add(Item);
-        Item = "";
+        get => _age;
+        set => this.RaiseAndSetIfChanged(ref _age, value);
+    }
+    public ObservableCollection<Person> Persons { get; set; } = new ObservableCollection<Person>();
+    public void AddPerson()
+    {
+        Persons.Add(new Person(Name,Age));
+        Name = "";
+        Age = 0;
     }
 }
---
+++ Practice/Avalonia/ObservableCollectionTest/ObservableCollectionTest/ViewModels/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionTest.ViewModels
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int weight)
+        {
+            Name = name;
+            Age = weight;
+        }
+    }
+}
